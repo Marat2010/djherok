1f1a37db6ace1822311a9c32656d9855db73ad01
Marat2010 <marat.s.73@mail.ru>
2019-11-02 22:37:55 +0300

    12121

--- weatherBot/views.py
+++ weatherBot/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import json
 
 URL = 'https://api.telegram.org/bot919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE/'
+
+
+def write_json(data, filename='answer.json'):
+    with open(filename, w) as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
 
 def index(request):
     # bot.polling(none_stop=True)
 
     r = requests.get(URL + 'getMe')
-    print(r.json())
+    write_json(r.json())
 
     return HttpResponse("<h1>Скрипт бота 'Test' </h1>" + str(r.json()))
 
