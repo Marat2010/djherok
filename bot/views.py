41f5792d8af8987ff1b8ba60dcd25a2de19dfb72
Marat2010 <marat.s.73@mail.ru>
2019-11-03 13:50:28 +0300

    make it better

---
+++ bot/views.py
@@ -0,0 +1,36 @@
+# Setting up Webhook
+# https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/setWebhook?url=https://mysite.com/cbbf15d8-0421-4512-84d9-5e5d977e3aef/
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
+from django.views.decorators.csrf import csrf_exempt
+import telebot
+
+TOKEN = '919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE'
+tbot = telebot.TeleBot(TOKEN)
+
+
+# For free PythonAnywhere accounts
+# tbot = telebot.TeleBot(TOKEN, threaded=False)
+
+
+@csrf_exempt
+def bot(request):
+    if request.META['CONTENT_TYPE'] == 'application/json':
+
+        json_data = request.body.decode('utf-8')
+        update = telebot.types.Update.de_json(json_data)
+        tbot.process_new_updates([update])
+
+        return HttpResponse("qwqw1")
+
+    else:
+        raise PermissionDenied
+
+
+@tbot.message_handler(content_types=["text"])
+def get_okn(message):
+    tbot.send_message(message.chat.id, "Hello, bot!")
+
