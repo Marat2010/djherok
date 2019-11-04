233cc76f1346cf7c7addbb7b75e7ac506b96d46e
Marat2010 <marat.s.73@mail.ru>
2019-11-05 02:26:04 +0300

    make it better

--- bot/views.py
+++ bot/views.py
@@ -10,6 +10,7 @@ from django.core.exceptions import PermissionDenied
 from django.views.decorators.csrf import csrf_exempt
 # import pyTelegramBotAPI
 import telebot
+import requests
 
 TOKEN = '919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE'
 tbot = telebot.TeleBot(TOKEN)
@@ -20,24 +21,24 @@ tbot = telebot.TeleBot(TOKEN)
 
 @csrf_exempt
 def bot(request):
+    URL = 'https://api.telegram.org/bot919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE/'
+    URL = URL + 'getMe'
+    r = requests.get(URL)
+
     # if request.META['CONTENT_TYPE'] == 'application/json':
     if request.content_type == 'application/json':
         json_data = request.body.decode('utf-8')
         update = telebot.types.Update.de_json(json_data)
         tbot.process_new_updates([update])
-        return HttpResponse("qwqw1")
+        return HttpResponse("qwqw1" + str(r))
     else:
         raise PermissionDenied
 
 
 @tbot.message_handler(content_types=["text"])
 def get_okn(message):
-    URL = 'https://api.telegram.org/bot919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE/'
-    URL = URL + 'getMe'
-    r = request.get(URL)
-
     tbot.send_message(message.chat.id, "--Hello--, bot!")
-    tbot.send_message(message.chat.id, r)
+    tbot.send_message(message.chat.id, 'r')
     return HttpResponse("--qwqw1---")
 
 
