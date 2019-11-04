4cac9c16991eb9ed6f7388c77f503921a13037a7
Marat2010 <marat.s.73@mail.ru>
2019-11-05 01:14:24 +0300

    make it better

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,8 @@
 # Setting up Webhook
-# https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/setWebhook?url=https://mysite.com/cbbf15d8-0421-4512-84d9-5e5d977e3aef/
-# https://api.telegram.org/bot919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE/setWebhook?url=https://edcd2e43.ngrok.io/bot/
+# https://api.telegram.org/bot919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE/setWebhook?url=https://djherok.herokuapp.com/bot/
+# https://api.telegram.org/bot919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE/getWebhookInfo
+# deleteWebhook     getWebhookInfo  setWebhook
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -10,12 +12,10 @@ from django.views.decorators.csrf import csrf_exempt
 import telebot
 
 TOKEN = '919974881:AAHwfCsrATbNx9fxjhbSxzacw5Ip-G-aTKE'
-# tbot = telebot.TeleBot(TOKEN)
 tbot = telebot.TeleBot(TOKEN)
 
 
-# For free PythonAnywhere accounts
-# tbot = telebot.TeleBot(TOKEN, threaded=False)
+# For free PythonAnywhere accounts  - # tbot = telebot.TeleBot(TOKEN, threaded=False)
 
 
 @csrf_exempt
@@ -35,4 +35,6 @@ def bot(request):
 @tbot.message_handler(content_types=["text"])
 def get_okn(message):
     tbot.send_message(message.chat.id, "Hello, bot!")
+    return HttpResponse("qwqw1")
+
 
