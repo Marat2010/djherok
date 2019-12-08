cfe7ce92b25c9e91d80f2c9811b197583b121454
Marat2010 <marat.s.73@mail.ru>
2019-12-08 08:10:55 +0300

    map1

--- weatherBot/views.py
+++ weatherBot/views.py
@@ -16,18 +16,29 @@ else:
 token_pyowm = os.environ['token_pyowm']
 file_answer = './weatherBot/answer.json'
 file_answer_city = './weatherBot/city.txt'
+file_data_bot = './weatherBot/data_bot.json'
 URL = 'https://api.telegram.org/bot' + token_telegram + '/'    # print(URL)
-owm = pyowm.OWM(token_pyowm, language='ru')
+# owm = pyowm.OWM(token_pyowm, language='ru')
 # proxies = {'https': 'https://178.32.55.52:443/', 'http': 'https://103.101.253.18:443/'}
 # proxies = {'https': 'https://ua-139-170-1.fri-gate0.biz:443/'}
 # Искать прокси с портом 443 ( https://awmproxy.com/freeproxy.php )
 proxies = {'https': 'https://70.89.113.137:443/'}
 
-icon_url = 'http://openweathermap.org/img/w/04n.png'
+
+def send_message(chat_id, text='--Привет, привет!-- )'):
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text}
+    # print("---УРЛ и Словарьв send_message перед отправкой: ", url, answer)
+    if local_launch:
+        r = requests.post(url, json=answer, proxies=proxies)  # add proxies on local
+    else:
+        r = requests.post(url, json=answer)  # remove proxies on hosting
+    return r
 
 
 def get_wind_direction(deg):
     direction = ['С ', 'СВ', ' В', 'ЮВ', 'Ю ', 'ЮЗ', ' З', 'СЗ']
+    res = "\U00002753"
     for i in range(0, 8):
         try:
             deg = int(deg)
@@ -57,14 +68,21 @@ def read_json(filename=file_answer):
 
 
 def get_icon(status='Неизвестно', weather_code=''):
-    dict_status = {'Clear': "\U00002600", 'Snow': "\U00002744",
+    """В 'get_icon' лучше убрать проверку 'status'. Сделать проверку только по 'weather_code'
+    в словаре со списком кодов, где вытащить ключ, который будет стикером 'Emoji Unicode'
+    ('https://unicode-table.com/', 'https://unicode.org/emoji/charts/full-emoji-list.html').
+        Описание кодов 'weather_code' 'https://openweathermap.org/weather-conditions'"""
+    dict_status = {'Clear': "\U00002600", 'Snow': "\U00002744", 'Heavy snowfall': "\U00002744\U00002744",
                    'Clouds': "\U00002601", 'Few clouds': "\U000026C5",
                    'Rain': "\U0001F327", 'Light rain': "\U00002602", 'Moderate rain': "\U00002614",
                    'Thunderstorm': "\U000026C8", 'Mist': "\U0001F301"}
-    # Туман Mist: мост-1F301, 1F32B, Снег:- U+1F328,  2744
+    # Туман Mist: мост-1F301, 1F32B, Снег:- U+1F328,  2744. Снег U+2746 \U00002744 ❅ ❅❄❅❄ chr(10054)
     if status == 'Clouds':
         if weather_code in [801, 802]:
             status = 'Few clouds'
+    if status == 'Snow':
+        if weather_code == 622:
+            status = 'Heavy snowfall'
     if status == 'Rain':
         if weather_code == 500:
             status = 'Light rain'
@@ -88,56 +106,202 @@ def decrease_record(record=''):
     return rec
 
 
-def send_message(chat_id, text='--Привет, привет!-- )'):
-    url = URL + 'sendMessage'
-    answer = {'chat_id': chat_id, 'text': text}
-    # print("---УРЛ и Словарьв send_message перед отправкой: ", url, answer)
-    if local_launch:
-        r = requests.post(url, json=answer, proxies=proxies)  # add proxies on local
-    else:
-        r = requests.post(url, json=answer)  # remove proxies on hosting
-    return r
-
-
-def answer_weather(message):
-    try:
-        owm.weather_at_place(message)
-    except pyowm.exceptions.api_response_error.NotFoundError:
-        answer_w = 'Такого города или места не знаю. Иностранные или некоторые города вводите на английском, ' \
-                    'например Сочи-Sochi, Киев-Kiev.'
-    else:
-        with open(file_answer_city, 'w') as f:   # write str city to file
-            f.write(message)
-        observation = owm.weather_at_place(message)
-        w = observation.get_weather()
-        date_w = w.get_reference_time(timeformat='date')
-        temp = w.get_temperature('celsius')["temp"]
+def city_coords(message):
+    ll = message.split(',')
+    if len(ll) > 1:
         try:
-            deg_wind = w.get_wind()['deg']
-        except KeyError:
-            deg_wind = "?"
-        answer_w = 'В городе {} {}, темп-ра: {:4.1f} C°\n'.format(w.get_detailed_status(),
-                                    get_icon(w.get_status(), w.get_weather_code()), temp)
-        answer_w += 'Ветер: {:3.1f} м/c ({}°-{})\n'.format(w.get_wind()["speed"],
-                                                           deg_wind,
-                                                           get_wind_direction(deg_wind))
-        answer_w += 'Влажн: {} %, Давл: {} мм.рт.ст.\n'.format(
-            w.get_humidity(),
-            int(w.get_pressure()["press"]/1.333224))
-        answer_w += 'Время(GMT+00): {}\n'.format(date_w.strftime("%H:%M %d.%m.%Y"))
-        answer_w += ' Где интересует погода или прогноз?:'
+            float(ll[1])
+        except ValueError:
+            return 'city'
+        else:
+            ll = [l.replace(' ', '') for l in ll]
+            return ll
+    else:
+        return 'city'
+
+
+def get_observation(message, language_code):
+    # global city_message
+    owm = pyowm.OWM(token_pyowm, language=language_code)
+    cit_coor = city_coords(message)
+    if cit_coor == 'city':
+        try:
+            owm.weather_at_place(message)
+        except Exception:
+            answer_w = 'Такого города или места не знаю.'
+            return answer_w
+        else:
+            # city_message = message
+            with open(file_answer_city, 'w') as f:  # write str city to file
+                f.write(message)
+
+            data_bot = read_json(file_data_bot)
+            data_bot["message"] = message
+            write_json(data_bot, file_data_bot)
+
+            observation = owm.weather_at_place(message)
+            return observation
+    else:
+        try:
+            owm.weather_at_coords(float(cit_coor[0]), float(cit_coor[1]))
+        # except pyowm.exceptions.api_response_error.NotFoundError:
+        except Exception:
+            answer_w = 'Такого места не знаю. Координаты не верны.'
+            return answer_w
+        else:
+            # city_message = message
+            with open(file_answer_city, 'w') as f:  # write str city to file
+                f.write(message)
+
+            data_bot = read_json(file_data_bot)
+            data_bot["message"] = message
+            write_json(data_bot, file_data_bot)
+
+            observation = owm.weather_at_coords(float(cit_coor[0]), float(cit_coor[1]))
+            return observation
+
+
+def record_answer(coords, w, message=''):
+    date_w = w.get_reference_time(timeformat='date')
+    temp = w.get_temperature('celsius')["temp"]
+    try:
+        deg_wind = w.get_wind()['deg']
+    except KeyError:
+        deg_wind = "?"
+    # key_temp = [key for key, val in w.get_temperature().items() if val == temp]
+    with open(file_answer_city, 'r') as f:
+        city_message = f.readline()
+    answer_w = '{}: {} {}, темп-ра: {:4.1f} C°\n'.format(
+                city_message, w.get_detailed_status(),
+                get_icon(w.get_status(), w.get_weather_code()), temp)
+    answer_w += 'Ветер: {:3.1f} м/c ({}°-{}).\n'.format(
+                w.get_wind()["speed"], deg_wind, get_wind_direction(deg_wind))
+    answer_w += 'Влажность: {} %.\nДавление: {} мм.рт.ст.\n'.format(
+                w.get_humidity(), int(w.get_pressure()["press"]/1.333224))
+    answer_w += 'Время(GMT+00): {}\n'.format(date_w.strftime("%H:%M %d.%m.%Y"))
+    answer_w += ' Широта: {}, Долгота: {}\n'.format(coords.get_lat(), coords.get_lon())
+    answer_w += ' Geonames: {}, ID:{}.'.format(coords.get_name(), coords.get_ID())
+
+    # if city_coords(message) == 'city':
+    #     lat = coords.get_lat()
+    #     lon = coords.get_lon()
+    # else:
+    #     lat = message.split(',')[0]
+    #     lon = message.split(',')[1]
+    # link = 'https://www.google.ru/maps/@{},{},15z\n'.format(lat, lon).replace(' ', '')
+    # answer_w += '\nНа карте: {}'.format(link)
     return answer_w
 
 
-def forecast(message, days_fc=5):
+def see_map(message):
+    data_bot = read_json(file_data_bot)
+    language_code = data_bot["language_code"]
+
+    observation = get_observation(message, language_code)
+    coords = observation.get_location()
+    if city_coords(message) == 'city':
+        lat = coords.get_lat()
+        lon = coords.get_lon()
+    else:
+        lat = message.split(',')[0]
+        lon = message.split(',')[1]
+    link = 'https://www.google.ru/maps/@{},{},15z\n'.format(lat, lon).replace(' ', '')
+    answer_map = '\nНа карте: {}'.format(link)
+    return answer_map
+
+
+def answer_weather(message, language_code):
+    observation = get_observation(message, language_code)
+    print("Объект наблюдения: ", observation, type(observation))
+    if isinstance(observation, str):
+        return observation  # Возврат сообщения о неудачи
+    else:
+        coords = observation.get_location()
+        w = observation.get_weather()
+        return record_answer(coords, w, message)  # Формирование сообщения
+
+# def answer_weather(message):
+#     try:
+#         owm.weather_at_place(message)
+#     except pyowm.exceptions.api_response_error.NotFoundError:
+#         answer_w = 'Такого города или места не знаю. Иностранные или некоторые города вводите на английском, ' \
+#                     'например Сочи-Sochi, Киев-Kiev.'
+#     else:
+#         with open(file_answer_city, 'w') as f:   # write str city to file
+#             f.write(message)
+#         observation = owm.weather_at_place(message)
+#         w = observation.get_weather()
+#         date_w = w.get_reference_time(timeformat='date')
+#         temp = w.get_temperature('celsius')["temp"]
+#         try:
+#             deg_wind = w.get_wind()['deg']
+#         except KeyError:
+#             deg_wind = "?"
+#         answer_w = 'В городе {} {}, темп-ра: {:4.1f} C°\n'.format(w.get_detailed_status(),
+#                                     get_icon(w.get_status(), w.get_weather_code()), temp)
+#         answer_w += 'Ветер: {:3.1f} м/c ({}°-{})\n'.format(w.get_wind()["speed"],
+#                                                            deg_wind,
+#                                                            get_wind_direction(deg_wind))
+#         answer_w += 'Влажн: {} %, Давл: {} мм.рт.ст.\n'.format(
+#             w.get_humidity(),
+#             int(w.get_pressure()["press"]/1.333224))
+#         answer_w += 'Время(GMT+00): {}\n'.format(date_w.strftime("%H:%M %d.%m.%Y"))
+#         answer_w += ' Где интересует погода или прогноз?:'
+#     return answer_w
+# ----------
+# def forecast(message, days_fc=5):
+#     try:
+#         fc = owm.three_hours_forecast(message)
+#         f = fc.get_forecast()
+#         lst = f.get_weathers()
+#     except pyowm.exceptions.api_response_error.NotFoundError:
+#         answer_fc = 'Введите сначала город.'
+#     except pyowm.exceptions.api_call_error.APICallError:
+#         answer_fc = '-Введите сначала город. Возможно проблема с сетью-'
+#     else:
+#         answer_fc = '{} (время по GMT+00):\n'.format(message)
+#         i = 0
+#         for w in lst:
+#             date_fc = w.get_reference_time(timeformat='date')
+#             try:
+#                 deg_wind = w.get_wind()['deg']
+#             except KeyError:
+#                 deg_wind = "?"
+#             answer_fc += '{}ч:{:2}{:5.1f}C°,{:4.1f}м/с({:3}°-{:>2})\n'.format(
+#                 date_fc.strftime("%d.%m %H"),
+#                 get_icon(w.get_status(), w.get_weather_code()),
+#                 w.get_temperature('celsius')["temp"],
+#                 w.get_wind()["speed"],
+#                 deg_wind,
+#                 get_wind_direction(deg_wind))
+#             if days_fc == 5:
+#                 answer_fc = answer_fc.rstrip('\n')
+#                 answer_fc += ', Вл:{:3}%, Давл:{:3}мм. {}\n'.format(
+#                     w.get_humidity(),
+#                     int(w.get_pressure()["press"]/1.333224),
+#                     decrease_record(w.get_detailed_status()))
+#             i += 1
+#             if i > (days_fc*8):
+#                 break
+#     return answer_fc
+
+
+def forecast(message, language_code, days_fc=5):
+    owm = pyowm.OWM(token_pyowm, language=language_code)
+    cit_coor = city_coords(message)
     try:
-        fc = owm.three_hours_forecast(message)
+        if cit_coor == 'city':
+            fc = owm.three_hours_forecast(message)
+        else:
+            observation = owm.weather_at_coords(float(cit_coor[0]), float(cit_coor[1]))
+            coords = observation.get_location()
+            fc = owm.three_hours_forecast(coords.get_name())
         f = fc.get_forecast()
         lst = f.get_weathers()
     except pyowm.exceptions.api_response_error.NotFoundError:
-        answer_fc = 'Введите сначала город.'
+        answer_fc = 'Введите сначала город или координаты.\n'
     except pyowm.exceptions.api_call_error.APICallError:
-        answer_fc = '-Введите сначала город. Возможно проблема с сетью-'
+        answer_fc = 'Не могу дать прогноз в этой точке. Введите ближайший город.\n'
     else:
         answer_fc = '{} (время по GMT+00):\n'.format(message)
         i = 0
@@ -156,14 +320,36 @@ def forecast(message, days_fc=5):
                 get_wind_direction(deg_wind))
             if days_fc == 5:
                 answer_fc = answer_fc.rstrip('\n')
-                answer_fc += ', Вл:{:3}%, Давл:{:3}мм. {}\n'.format(
+                # answer_fc += ', Вл:{:3}%, Давл:{:3}мм. {:>6} - {:>10} :{:4}-{:4}\n'.format(
+                answer_fc += ', Вл:{:3}%, Давл:{:3}мм. {:>10}:\n'.format(
                     w.get_humidity(),
                     int(w.get_pressure()["press"]/1.333224),
+                    # w.get_weather_icon_name(), w.get_status(), w.get_weather_code())
                     decrease_record(w.get_detailed_status()))
             i += 1
-            if i > (days_fc*8):
+            if i > (days_fc * 8):
                 break
     return answer_fc
+
+
+def data_bot_rw(rw, **kwargs):
+    pass
+    # if rw == 'write':
+    #     data_bot = read_json(file_data_bot)
+    #
+    #     data_bot["chat][id"] = chat_id
+    #
+    #     data_bot["language_code"] = language_code
+    #     data_bot["chat]["message"] = message
+    #     write_json(data_bot, file_data_bot)
+    # else:
+    #     data_bot = read_json(file_data_bot)
+    #     return data_bot
+    #
+        # if data_bot["language_change"] == 'True':
+        #     language_code = data_bot["language_code"]
+        # else:
+        #     pass
 
 
 @django.views.decorators.csrf.csrf_exempt
@@ -177,6 +363,32 @@ def index(request):
         write_json(r)
         chat_id = r['message']['chat']['id']
         message = r['message']['text']
+        try:
+            language_code = r['message']['from']['language_code']
+        except KeyError:
+            # language_code = 'en'
+            language_code = ''
+        try:
+            data_bot = read_json(file_data_bot)
+            if data_bot["language_change"] == 'True':
+                language_code = data_bot["language_code"]
+            else:
+                data_bot["language_code"] = language_code
+            data_bot["chat_id"] = chat_id
+            data_bot["message"] = message
+            write_json(data_bot, file_data_bot)
+        except Exception as e:
+            data_bot = {}
+            data_bot["chat_id"] = chat_id
+            data_bot["message"] = message
+            write_json(data_bot, file_data_bot)
+
+        print('---код lang в начале:', language_code)
+
+        # owm = pyowm.OWM(token_pyowm, language='ru')
+        # language_code = "ru"
+        # owm = pyowm.OWM(token_pyowm, language=language_code)
+
         print("---Чат ID и тек сообщ:", chat_id, type(chat_id), message, type(message))
         if '/start' in message:
             answer = 'Привет, {}.\n/help для помощи'.format(r['message']['chat']['first_name'])
@@ -190,11 +402,35 @@ def index(request):
                      'И не забываем, время по Гринвичу (GMT+00).'
         elif message in ['/fc_short', 'S']:
             days_fc = 2
-            answer = forecast(city_message, days_fc)
+            answer = forecast(city_message, language_code, days_fc)
         elif message in ['/fc_full', 'F']:
-            answer = forecast(city_message)
+            answer = forecast(city_message, language_code)
+        elif message in ['/map', 'M']:
+            answer = see_map(city_message)
+        elif '/l' in message:
+            try:
+                if message.find(',') == -1:
+                    ll = message.split()
+                else:
+                    ll = message.split(',')
+                ll = [l.replace(' ', '') for l in ll]
+                language_code = ll[1]
+                # res = [x for x in lst if x % 3 == 0]
+            except Exception:
+                answer = "Не верно введено!"
+                language_code = 'en'
+            else:
+                data_bot = read_json(file_data_bot)
+                data_bot["language_code"] = language_code
+                data_bot["language_change"] = "True"
+                write_json(data_bot, file_data_bot)
+
+                answer = 'Изменен язык на "{}"\n ru,en,zh-китай,uk-украин,de-нем,' \
+                         'fr-франц,it-италь,es-испан,tr-турец...\n'.format(language_code)
+            owm = pyowm.OWM(token_pyowm, language=language_code)
+            print(owm)
         else:
-            answer = answer_weather(message)
+            answer = answer_weather(message, language_code)
         r = send_message(chat_id, text=answer)
         return HttpResponse(r, content_type="application/json")
     else:
@@ -206,8 +442,13 @@ def index(request):
                             content_type="application/json")
 
 
-if __name__ == '__main__':
-    pass
-
-
+# if __name__ == '__main__':
+#     pass
 # ------------
+
+
+
+
+
+
+
