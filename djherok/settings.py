ceb1d7dd0b0d2b091a709266d16c4df1cff9d00b
Marat2010 <marat.s.73@mail.ru>
2020-05-08 04:39:42 +0300

    go postgresql

--- djherok/settings.py
+++ djherok/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,13 +97,33 @@ WSGI_APPLICATION = 'djherok.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'TIME_ZONE': 'Europe/Moscow',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TIME_ZONE': 'Europe/Moscow',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djherokbd',
+        # 'NAME': os.environ['DB_NAME'],
+        'USER': 'djherokuser',
+        # 'USER': os.environ['DB_USER'],
+        'PASSWORD': '091973psql16',
+        # 'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
+        # 'HOST' : '127.0.0.1',
+        # 'PORT' : '5432',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
